312db7eea434589e4b6a41f57ca44eedb7622ff4
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-06-18 21:04:34 -0400

    Add helper method to extract unique words

--- src/swipe_extractor.py
+++ src/swipe_extractor.py
@@ -13,6 +13,19 @@ def grab_first():
     return df
 
 
+def unique_words(df: pd.DataFrame):
+    data = df.iloc[:, :1].values.tolist()
+    store = []
+    for row in data:
+        # Since the log file is not actually a csv we can't do a simple column name/index lookup
+        string = row[10]
+        sep = " "
+        # Use a regex of the space character to split out the sentence column from the string
+        sentence = string.split(sep, 1)[0]
+        store.append(sentence)
+    return set(store)
+
+
 def unique_sentences(df: pd.DataFrame):
     data = df.iloc[:, :1].values.tolist()
     store = []
