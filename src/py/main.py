b8e116235fed2578d6232a517729021c141a9c79
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-06-30 22:17:16 -0400

    Prepare for refactoring

--- src/py/main.py
+++ src/py/main.py
@@ -1,8 +1,5 @@
-from swipe_extractor import grab_first, into_swipe_set
+from swipe_extractor import grab_first
 
 
 if __name__ == "__main__":
     df = grab_first()
-    swipe_set = into_swipe_set(df)
-    for swipe in swipe_set:
-        print(swipe.stringify())
--- src/py/swipe_extractor.py
+++ src/py/swipe_extractor.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import os
 
-from swipe import Swipe, SwipeSet
-
 
 def grab_first():
     path = os.path.join(os.getcwd(), "data")
@@ -37,18 +35,3 @@ def unique_sentences(df: pd.DataFrame):
         sentence = string.split(sep, 1)[0]
         store.append(sentence)
     return set(store)
-
-
-def into_swipe_set(df: pd.DataFrame):
-    data = df.iloc[:, :1].values.tolist()
-    unique = unique_sentences(df)
-    store = []
-    for sentence in unique:
-        for row in data:
-            string = row[0]
-            sep = " "
-            found = string.split(sep, 1)[0]
-            remaining = str(string.split(sep, 1)[1:])
-            if found == sentence:
-                store.append(Swipe(sentence, remaining))
-    return store
