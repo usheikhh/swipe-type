108a1ffca1dedbff0dce3ed4b1f84c57d3790c23
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-07-01 20:12:13 -0400

    First pass implementation of swipe execution
    This commit implements the untested core of the swipe execution
    algorithm. The next step is to implement a new Swipe data structure
    and integrate the algorithm with it.

--- src/py/main.py
+++ src/py/main.py
@@ -1,5 +1,22 @@
+from swipe_extractor import (
+    compute_timestamp_deltas,
+    extract_timestamps,
+    extract_swipes_indices,
+    into_intervals,
+    create_swipes,
+)
 from swipe_extractor import grab_first
+import os
 
 
 if __name__ == "__main__":
-    df = grab_first()
+    timestamps, word = extract_timestamps(
+        os.path.join(os.getcwd(), "src", "py", "delay.log")
+    )
+    print(word)
+    delta = compute_timestamp_deltas(timestamps)
+    indices = extract_swipes_indices(delta)
+    intervals = into_intervals(indices)
+    swipes = create_swipes(timestamps, word, intervals)
+    for swipe in swipes:
+        print(len(swipe[1]))
--- src/py/swipe_extractor.py
+++ src/py/swipe_extractor.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import os
+from typing import List
+from swipe import Swipe
+
+THRESHOLD = 30
 
 
 def grab_first():
@@ -35,3 +39,69 @@ def unique_sentences(df: pd.DataFrame):
         sentence = string.split(sep, 1)[0]
         store.append(sentence)
     return set(store)
+
+
+def extract_timestamps(path: str):
+    file = open(path, "r")
+    lines = file.readlines()
+    timestamps = []
+    for line in lines:
+        res = list(line.split(" "))[1]
+        word = list(line.split(" "))[10]
+        timestamps.append((res))
+    return (timestamps[1:], word)
+
+
+def compute_timestamp_deltas(timestamps: List[int]):
+    curr = int(timestamps[0])
+    # print("curr", curr)
+    deltas = []
+    for i in range(1, len(timestamps)):
+        delta = int(timestamps[i]) - curr
+        deltas.append(delta)
+        curr = int(timestamps[i])
+    return deltas
+
+
+def extract_swipes_indices(deltas: List[int]):
+    above = []
+    for i in range(0, len(deltas)):
+        if deltas[i] >= THRESHOLD:
+            above.append(i)
+    return above
+
+
+def into_intervals(indices: List[int]):
+    intervals = []
+    if len(indices) == 0:
+        raise ValueError("No indices provided")
+    if len(indices) == 1:
+        interval = [0, indices[0]]
+        intervals.append(interval)
+        return intervals
+    starting_index = 0
+    for index in indices:
+        interval = [starting_index, index]
+        intervals.append(interval)
+        starting_index = index + 1
+    return intervals
+
+
+def create_swipes(timestamps: List[str], word: str, intervals):
+    ranges = []
+    for interval in intervals:
+        ranges.append(list(range(interval[0], interval[1] + 1)))
+    # print(ranges)
+    swipe_list = []
+    times = []
+    for index_range in ranges:
+        for element in index_range:
+            time = timestamps[element]
+            times.append(time)
+        swipe = [word, times]
+        # print(len(times))
+        # print(swipe)
+        swipe_list.append(swipe)
+        # print(len(swipe_list))
+        times = []
+    return swipe_list
