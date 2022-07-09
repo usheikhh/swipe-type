bf4bb2d4d4c68434c3cc5919dd6bccf2f96fcd1b
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-07-08 23:34:00 -0400

    Fix bugs in swipe data structure accessors

--- src/py/swipe.py
+++ src/py/swipe.py
@@ -13,7 +13,8 @@ class Backing_File:
         return self.path
 
     def lookup_row_by_timestamp(self, timestamp: str):
-        f = open(self.backing_file.get_path(), "r")
+        # f = open(self.backing_file.get_path(), "r")
+        f = open(self.get_path(), "r")
         lines = f.readlines()
         for line in lines:
             if line.split(" ")[1] == timestamp:
@@ -31,18 +32,21 @@ class Swipe:
 
     def x_pos(self, timestamp: str):
         row = self.get_backing_file().lookup_row_by_timestamp(timestamp)
+        row = row.split(" ")
         return row[5]
 
     def y_pos(self, timestamp: str):
         row = self.get_backing_file().lookup_row_by_timestamp(timestamp)
+        row = row.split(" ")
         return row[6]
 
     def sentence(self, timestamp: str):
         row = self.get_backing_file().lookup_row_by_timestamp(timestamp)
+        row = row.split(" ")
         return row[0]
 
     def first_and_last_timestamp(self):
-        return (self.swipe_data[0], self.swipe_data[-1])
+        return [self.swipe_data[0], self.swipe_data[-1]]
 
     def last_timestamp(self):
         return self.swipe_data[-1]
@@ -52,18 +56,22 @@ class Swipe:
 
     def event(self, timestamp: str):
         row = self.get_backing_file().lookup_row_by_timestamp(timestamp)
+        row = row.split(" ")
         return row[4]
 
     def x_radius(self, timestamp: str):
         row = self.get_backing_file().lookup_row_by_timestamp(timestamp)
+        row = row.split(" ")
         return row[7]
 
     def y_radius(self, timestamp: str):
         row = self.get_backing_file().lookup_row_by_timestamp(timestamp)
+        row = row.split(" ")
         return row[8]
 
     def get_word(self, timestamp: str):
         row = self.get_backing_file().lookup_row_by_timestamp(timestamp)
+        row = row.split(" ")
         return row[10]
 
     def swipe_timestamps(self):
@@ -75,5 +83,5 @@ class Swipe:
     def stringify(self):
         return f"Word: {self.key} Timestamps {self.swipe_data}"
 
-    def get_backing_file(self):
+    def get_backing_file(self) -> Backing_File:
         return self.backing_file
