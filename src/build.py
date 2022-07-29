6e18386ccda414c3f998cb337b502944db9e4783
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-07-29 15:25:52 -0400

    Remove old genuine_scores folder

--- src/build.py
+++ src/build.py
@@ -17,14 +17,14 @@ if __name__ == "__main__":
     log.km_info("Creating DET curve...")
     s = 0
     genuine_scores_list = flatten(
-        list(loadall("/Users/alvinkuruvilla/Dev/swipe-type/aws-genuine.dat"))
+        list(loadall("/Users/alvinkuruvilla/Dev/swipe-type/exp4-genuine.dat"))
     )
     impostor_scores_list = []
-    p = os.path.join(os.getcwd(), "impostors_data")
+    p = os.path.join(os.getcwd(), "exp4-gen")
     folder_names = [f for f in os.listdir(p) if os.path.isdir(os.path.join(p, f))]
     # print(folder_names)
     for folder_name in folder_names:
-        p2 = os.path.join(os.getcwd(), "impostors_data", folder_name)
+        p2 = os.path.join(os.getcwd(), "exp4-gen", folder_name)
         file_names = [f for f in os.listdir(p2) if os.path.isfile(os.path.join(p2, f))]
         for file in file_names:
             # print(os.path.join(p2, file))
